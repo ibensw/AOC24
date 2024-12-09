31c93cec2f2bf23f7ff7c5164e5aa907b87ab637
Wouter ibens <wouter.ibens@xemex.eu>
2024-12-09 15:04:52 +0100

    day1

---
+++ day1/part2.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <filesystem>
+#include <fmt/format.h>
+#include <fstream>
+#include <ranges>
+#include <utility>
+#include <vector>
+
+std::pair<std::vector<unsigned>, std::vector<unsigned>> parseInput(const std::filesystem::path &path)
+{
+    std::vector<unsigned> left;
+    std::vector<unsigned> right;
+
+    std::ifstream inputFile(path);
+
+    if (!inputFile.is_open()) {
+        throw std::runtime_error("Failed to open input file");
+    }
+
+    while (!inputFile.eof()) {
+        unsigned l;
+        unsigned r;
+        inputFile >> l >> r;
+        if (!inputFile.fail()) {
+            left.push_back(l);
+            right.push_back(r);
+        }
+    }
+
+    return std::make_pair(left, right);
+}
+
+unsigned findTotalDiff(std::pair<std::vector<unsigned>, std::vector<unsigned>> &input)
+{
+    auto diff = [](unsigned a, unsigned b) { return a > b ? a - b : b - a; };
+
+    auto &[left, right] = input;
+    std::ranges::sort(left);
+    std::ranges::sort(right);
+
+    auto startLeft = left.begin();
+    auto startRight = right.begin();
+
+    unsigned totalDiff = 0;
+
+    while (startLeft != left.end() && startRight != right.end()) {
+        auto smallest = std::min(*startLeft, *startRight);
+
+        auto lend = std::upper_bound(startLeft, left.end(), smallest);
+        auto nl = std::distance(startLeft, lend);
+        auto rend = std::upper_bound(startRight, right.end(), smallest);
+        auto nr = std::distance(startRight, rend);
+        totalDiff += smallest * nl * nr;
+
+        startLeft = lend;
+        startRight = rend;
+    }
+
+    return totalDiff;
+}
+
+int main(int argc, char *argv[])
+{
+    auto input = parseInput(argv[1]);
+    fmt::print("Total difference: {}\n", findTotalDiff(input));
+}
